10220d1af2cc11e61c7522bf0c99cc88824be321
Eugeny Vlasov <eugenyvlasov@gmail.com>
2024-04-28 21:20:14 +0300

    Now lots info is taken from MySQL!

---
+++ configdb.php
@@ -0,0 +1,6 @@
+<?php
+    $mysql_host = 'localhost';
+    $mysql_login = 'root';
+    $mysql_password = 'mysql266';
+    $mysql_db = 'yeticavedb';
+?>
--- index.php
+++ index.php
@@ -2,9 +2,36 @@
     require_once './functions.php';
     require_once './data.php';
     require_once './auth.php';
+    require_once './configdb.php';
 
+    $connection = mysqli_connect($mysql_host, $mysql_login, $mysql_password, $mysql_db);
+    if($connection){
+        $sql = 'SELECT  Lots.name as name,
+                        Categories.Name AS category,
+                        Lots.Price as price,
+                        Lots.Step as step,
+                        Lots.Img as img,
+                        Lots.Enddate as enddate,
+                        Lots.Description as description
+                FROM Lots
+                JOIN Categories
+                ON Lots.CategoryId = Categories.id
+                -- WHERE Lots.Enddate > CURDATE()
+                ORDER BY Lots.Startdate DESC';
+        $result = mysqli_query($connection, $sql);
 
-    $page_content = renderTemplate('./templates/index.php', ['lots' => $lots]);
+        if($result){
+            $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            $page_content = renderTemplate('./templates/index.php', ['lots' => $lots]);
+        }
+        else{
+            $page_content = renderTemplate('./templates/error.php', [ $error['mysql'] => mysqli_error($connection)] );
+        }
+    }
+    else{
+        $page_content = renderTemplate('./templates/error.php', [ $error['mysql'] => mysqli_connect_error()] );
+    }
+
     $layout_content = renderTemplate('./templates/layout.php',
         [
             'categories' => $categories,
